38500bcf2b09208c37c19dad95eefb2471cd5899
edgar <gesarki@gmail.com>
2020-03-20 01:15:23 -0400

    added copy-to-cliboard button to email signature generator, formated code following .editorconfig

--- assets/js/email-signature-generator.js
+++ assets/js/email-signature-generator.js
@@ -6,3 +6,37 @@ function generatees() {
     document.getElementById("fillLinkedin").href = document.getElementById("linkedinURL").value;
     document.getElementById("fillLinkedin").innerHTML = document.getElementById("linkedinUsername").value;
 }
+
+function copyes() {
+
+    let btn = $("#copybtn");
+
+    var originalText = btn.attr('data-original-title');
+
+    let sel, range;
+    let el = document.getElementById("selectAll"); //get element id
+    if (window.getSelection && document.createRange) { //Browser compatibility
+        sel = window.getSelection();
+        if (sel.toString() == '') { //no text selection
+            range = document.createRange(); //range object
+            range.selectNodeContents(el); //sets Range
+            sel.removeAllRanges(); //remove all ranges from selection
+            sel.addRange(range);//add Range to a Selection.
+        }
+    } else if (document.selection) { //older ie
+        sel = document.selection.createRange();
+        if (sel.text == '') { //no text selection
+            range = document.body.createTextRange();//Creates TextRange object
+            range.moveToElementText(el);//sets Range
+            range.select(); //make selection.
+        }
+    }
+    let successful = document.execCommand('copy');
+    let msg = successful ? "Copied!" : "Didn't work :(";
+
+    // set the tooltip to "copied" and re-draw it
+    $("#copybtn").attr("data-original-title", msg).tooltip("show");
+
+    // replace previous tooltip text
+    btn.attr('data-original-title', originalText);
+}
